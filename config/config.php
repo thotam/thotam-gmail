74d45ccfcd684b4532b669299dc64d0c4d0763b2
thotam <thanhtamtqno1@gmail.com>
2022-01-20 15:20:22 +0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -9,6 +9,7 @@ return [
         'clientSecret' => env('DEFAULT_GOOGLE_MAIL_CLIENT_SECRET', null),
         'clientId' => env('DEFAULT_GOOGLE_MAIL_CLIENT_ID', null),
         'surveyRefreshToken' => env('SurveyMail_GOOGLE_MAIL_REFRESH_TOKEN', null),
+        'kaizenRefreshToken' => env('KaizenMail_GOOGLE_MAIL_REFRESH_TOKEN', null),
         'buddyRefreshToken' => env('BuddyMail_GOOGLE_MAIL_REFRESH_TOKEN', null),
         'kbytRefreshToken' => env('KbytMail_GOOGLE_MAIL_REFRESH_TOKEN', null),
     ]
